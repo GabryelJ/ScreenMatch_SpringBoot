e1ddfbecf2e869b571a13da883329cdeecb48867
Gabryel <gabryeljuliods@gmail.com>
2025-06-15 19:39:10 -0300

    Ajustes config, service APIConsumption. Requisição de uma série.

--- src/main/java/dev/gabryel/screenmatch/ScreenmatchApplication.java
+++ src/main/java/dev/gabryel/screenmatch/ScreenmatchApplication.java
@@ -1,13 +1,29 @@
 package dev.gabryel.screenmatch;
 
+import dev.gabryel.screenmatch.service.APIConsumption;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ScreenmatchApplication {
+public class ScreenmatchApplication implements CommandLineRunner {
+
+	private final APIConsumption apiConsumption;
+
+	@Autowired
+	public ScreenmatchApplication(APIConsumption apiConsumption) {
+		this.apiConsumption = apiConsumption;
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(ScreenmatchApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		String mediaName = "The Office";
+		var json = apiConsumption.fetchData(mediaName);
+		System.out.println(json);
+	}
 }
---
+++ src/main/java/dev/gabryel/screenmatch/service/APIConsumption.java
@@ -0,0 +1,37 @@
+package dev.gabryel.screenmatch.service;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
+@Service
+public class APIConsumption {
+
+    @Value("${api.key}")
+    private String apiKey;
+
+    public String fetchData(String searchName) {
+        String address = "http://www.omdbapi.com/?t=" + searchName.replace(" ", "+") + '&' + "apikey=" + apiKey;
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create(address))
+                .build();
+        HttpResponse<String> response = null;
+        try {
+            response = client
+                    .send(request, HttpResponse.BodyHandlers.ofString());
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+
+        String json = response.body();
+        return json;
+    }
+}
