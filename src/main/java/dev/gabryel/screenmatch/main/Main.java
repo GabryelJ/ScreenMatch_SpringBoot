151887704346bde4a0e3c45ae833571576590255
GabryelJ <gabryeljuliods@gmail.com>
2025-07-16 19:14:17 -0300

    Stream statistics e busca de temporada por trecho de nome de episódio

--- src/main/java/dev/gabryel/screenmatch/main/Main.java
+++ src/main/java/dev/gabryel/screenmatch/main/Main.java
@@ -55,10 +55,11 @@ public class Main {
         episodes.forEach(System.out::println);
 
 
-        System.out.println("Top 5 episodios com temporada: ");
+        System.out.println("Top 10 episodios com temporada: ");
         episodes.stream()
                 .sorted(Comparator.comparing((Episode::getRating)).reversed())
-                .limit(5)
+                .limit(10)
+                .map(episode -> episode.getTitle().toUpperCase())
                 .forEach(System.out::println);
 
         System.out.println("A partir de que ano você deseja ver os episodios?");
@@ -74,5 +75,37 @@ public class Main {
                                 " Episodio: " + episode.getTitle() +
                                 " Data lancamento: " + episode.getReleaseDate().format(dateTimeFormatter)
                 ));
+
+
+        System.out.println("Para descobrir a temporada de um episódio que você não se lembra do nome completo, digite um trecho do titulo do episódio: ");
+
+        input.nextLine();
+        var titlePart = input.nextLine();
+
+        Optional<Episode> foundEpisode = episodes.stream()
+                    .filter(episode -> episode.getTitle().toUpperCase().contains(titlePart.toUpperCase()))
+                    .findFirst();
+
+        if (foundEpisode.isPresent()){
+            System.out.println("Episódio encontrado!");
+            System.out.println("Temporada: " + foundEpisode.get().getSeason());
+        }else{
+            System.out.println("Episódio não encontrado!");
+        }
+
+        Map<Integer, Double> ratingPerSeason = episodes.stream()
+                .filter(episode -> episode.getRating() > 0.0)
+                .collect(Collectors.groupingBy(Episode::getSeason, Collectors.averagingDouble(Episode::getRating)));
+
+        System.out.println(ratingPerSeason);
+
+        DoubleSummaryStatistics doubleSummaryStatistics = episodes.stream()
+                .filter(episode -> episode.getRating() > 0.0)
+                .collect(Collectors.summarizingDouble(Episode::getRating));
+
+        System.out.println("Média: " + doubleSummaryStatistics.getAverage());
+        System.out.println("Melhor episódio: " + doubleSummaryStatistics.getMax());
+        System.out.println("Pior episódio: " + doubleSummaryStatistics.getMin());
+        System.out.println("Quantidade de episódios:  " + doubleSummaryStatistics.getCount());
     }
 }
