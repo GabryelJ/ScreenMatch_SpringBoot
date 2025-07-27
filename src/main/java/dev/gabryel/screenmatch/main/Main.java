c5b6bc02226aa08e15b894a9c9c77a15a3502f61
GabryelJ <gabryeljuliods@gmail.com>
2025-07-26 21:02:24 -0300

    Menu.
    - Ajustes Main.
    - Adição de menu com switch.

--- src/main/java/dev/gabryel/screenmatch/main/Main.java
+++ src/main/java/dev/gabryel/screenmatch/main/Main.java
@@ -1,16 +1,13 @@
 package dev.gabryel.screenmatch.main;
 
-import dev.gabryel.screenmatch.model.Episode;
-import dev.gabryel.screenmatch.model.EpisodeData;
 import dev.gabryel.screenmatch.model.SeasonData;
 import dev.gabryel.screenmatch.model.SerieData;
 import dev.gabryel.screenmatch.service.APIConsumption;
 import dev.gabryel.screenmatch.service.DataParser;
 
-import java.time.LocalDate;
-import java.time.format.DateTimeFormatter;
-import java.util.*;
-import java.util.stream.Collectors;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
 
 public class Main {
 
@@ -25,87 +22,55 @@ public class Main {
         this.API_KEY = apiKey;
     }
 
-    public void showMenu(){
-        System.out.println("Digite o nome da serie: ");
-        var serieName = input.nextLine();
-        var json = apiConsumption.fetchData(ADDRESS + serieName.replace(' ', '+')+ "&apikey=" + API_KEY);
-        SerieData data = dataParser.getData(json, SerieData.class);
-        System.out.println(data);
+    public void showMenu() {
+        var menu = """
+                1 - Buscar séries
+                2 - Buscar episódios
+                
+                0 - Sair                                 
+                """;
 
+        System.out.println(menu);
+        var opcao = input.nextInt();
+        input.nextLine();
+
+        switch (opcao) {
+            case 1:
+                searchWebSerie();
+                break;
+            case 2:
+                searchEpisodeForSerie();
+                break;
+            case 0:
+                System.out.println("Saindo...");
+                break;
+            default:
+                System.out.println("Opção inválida");
+        }
+    }
+
+    private void searchWebSerie() {
+        SerieData data = getSerieData();
+        System.out.println(data);
+    }
+
+    private SerieData getSerieData() {
+        System.out.println("Digite o nome da série para busca");
+        var serieName = input.nextLine();
+        var json = apiConsumption.fetchData(ADDRESS + serieName.replace(" ", "+") + '&' + "apikey=" + API_KEY);
+        SerieData data = dataParser.getData(json, SerieData.class);
+        return data;
+    }
+
+    private void searchEpisodeForSerie(){
+        SerieData serieData = getSerieData();
         List<SeasonData> seasons = new ArrayList<>();
 
-        for (int i = 1; i < data.totalSeasons(); i++){
-            json = apiConsumption.fetchData("http://www.omdbapi.com/?t=" + serieName.replace(" ", "+") + '&' + "season=" + i + '&' + "apikey=" + API_KEY);
-            SeasonData seasonData = dataParser.getData(json, SeasonData.class);
-            seasons.add(seasonData);
+        for (int i = 1; i <= serieData.totalSeasons(); i++) {
+            var json = apiConsumption.fetchData(ADDRESS + serieData.title().replace(" ", "+") + "&season=" + i + '&' + "apikey=" + API_KEY);
+            SeasonData dadosTemporada = dataParser.getData(json, SeasonData.class);
+            seasons.add(dadosTemporada);
         }
-
         seasons.forEach(System.out::println);
-
-        seasons.forEach(season -> season.episodes()
-                .forEach(episode -> System.out.println(episode.title())));
-
-
-        List<Episode> episodes = seasons.stream()
-                .flatMap(season -> season.episodes().stream()
-                        .map(episodeData -> new Episode(season.number(), episodeData))
-                )
-                .collect(Collectors.toList());
-
-        episodes.forEach(System.out::println);
-
-
-        System.out.println("Top 10 episodios com temporada: ");
-        episodes.stream()
-                .sorted(Comparator.comparing((Episode::getRating)).reversed())
-                .limit(10)
-                .map(episode -> episode.getTitle().toUpperCase())
-                .forEach(System.out::println);
-
-        System.out.println("A partir de que ano você deseja ver os episodios?");
-        var year = input.nextInt();
-
-        LocalDate searchDate = LocalDate.of(year, 1, 1);
-
-        DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
-        episodes.stream()
-                .filter(episode -> episode.getReleaseDate() != null && episode.getReleaseDate().isAfter(searchDate))
-                .forEach(episode -> System.out.println(
-                        "Temporada: " + episode.getSeason() +
-                                " Episodio: " + episode.getTitle() +
-                                " Data lancamento: " + episode.getReleaseDate().format(dateTimeFormatter)
-                ));
-
-
-        System.out.println("Para descobrir a temporada de um episódio que você não se lembra do nome completo, digite um trecho do titulo do episódio: ");
-
-        input.nextLine();
-        var titlePart = input.nextLine();
-
-        Optional<Episode> foundEpisode = episodes.stream()
-                    .filter(episode -> episode.getTitle().toUpperCase().contains(titlePart.toUpperCase()))
-                    .findFirst();
-
-        if (foundEpisode.isPresent()){
-            System.out.println("Episódio encontrado!");
-            System.out.println("Temporada: " + foundEpisode.get().getSeason());
-        }else{
-            System.out.println("Episódio não encontrado!");
-        }
-
-        Map<Integer, Double> ratingPerSeason = episodes.stream()
-                .filter(episode -> episode.getRating() > 0.0)
-                .collect(Collectors.groupingBy(Episode::getSeason, Collectors.averagingDouble(Episode::getRating)));
-
-        System.out.println(ratingPerSeason);
-
-        DoubleSummaryStatistics doubleSummaryStatistics = episodes.stream()
-                .filter(episode -> episode.getRating() > 0.0)
-                .collect(Collectors.summarizingDouble(Episode::getRating));
-
-        System.out.println("Média: " + doubleSummaryStatistics.getAverage());
-        System.out.println("Melhor episódio: " + doubleSummaryStatistics.getMax());
-        System.out.println("Pior episódio: " + doubleSummaryStatistics.getMin());
-        System.out.println("Quantidade de episódios:  " + doubleSummaryStatistics.getCount());
     }
 }
